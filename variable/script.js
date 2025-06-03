3705a01d8902216c19ae37c90c4ef402f662203e
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-03 22:23:46 +0600

    About const variable

--- variable/script.js
+++ variable/script.js
@@ -9,3 +9,23 @@ let x = 50;
 }
 
 document.getElementById('demo1').innerHTML = x;
+
+
+// Constent 
+
+const numbers = [1,2,3,4]; 
+// numbers = [5,6];  // This will be show an error.  You can not reassign array
+numbers.push(5);
+numbers[0] = 10; // Change the elements of constant array
+numbers[3] = 30; // Change the elements of constant array
+
+console.log(numbers);
+
+// Same as objec 
+const person = {name: 'Sanowar'}
+// person = {age: 30} // This will be show an error.  invalid assignment to const 'person'
+person.age = 40; // Change the elements of constant object
+person.name = 'Mobin'; // Change the elements of constant object
+
+
+console.log(person);
