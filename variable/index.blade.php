883f603c5b281a28d8daa0b27bb1bef5092a10b9
devsanowar <mdsanowarhossen.cse@gmail.com>
2025-06-03 21:52:48 +0600

    let variable not redeclare but its possible to assign

---
+++ variable/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript Fundamental</title>
+</head>
+<body>
+    
+    <h2 id="demo1"></h2>
+    <p id="demo2"></p>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ variable/script.js
@@ -0,0 +1,11 @@
+let x = 50;
+// let   x = 20;
+
+{
+let x = 50;
+// let   x = 20
+    //  document.getElementById('demo2').innerHTML = x;
+    console.log(x);
+}
+
+document.getElementById('demo1').innerHTML = x;
